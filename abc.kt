343edbd6382b9712f19e8886f4449ed8ad84fe4f
Atul Yadav <atulyadav2580@gmail.com>
2021-07-06 13:26:59 +0530

    second  change

---
+++ abc.kt
@@ -0,0 +1,30 @@
+import jdk.jshell.execution.Util
+import org.w3c.dom.ranges.Range
+
+//1qaz0plm
+abstract class a {
+    var a = 12
+    open fun abc()
+    {
+        print("abc")
+    }
+}
+class b:a(){
+    var b = 12
+    var c = b.shr(12)
+
+
+    fun bc()
+    {
+        print("bc")
+    }
+}
+fun main(args: Array<String>){
+    var a = mutableListOf<Int>()
+    a.addAll(Array(10){-1})
+    for (i in 0 until(10) step 2)
+        println("$i ${a[i]}")
+}
+
+
+
