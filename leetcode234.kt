1cf6a5e2f78b28ad70496847fe783b48dd3dfe53
atul.yadav <atul.yadav@verse.in>
2021-07-19 17:29:45 +0530

    first commit

---
+++ leetcode234.kt
@@ -0,0 +1,29 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * */
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun isPalindrome(head: ListNode?): Boolean {
+        var a = ArrayList<Int>()
+        var b = head
+        while (b != null)
+        {
+            a.add(b.`val`)
+            b = b.next
+        }
+        var x = 0; var y = a.size-1;
+        while (x<y)
+        {
+            if (a.elementAt(x)!= a.elementAt(y))
+                return false
+            x++;y--;
+        }
+        return true
+    }
+}
