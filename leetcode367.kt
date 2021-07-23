7934a0ebe1dfb9a4fc7e68bc6930c63238175b27
atul.yadav <atul.yadav@verse.in>
2021-07-23 17:32:42 +0530

    solution 367

---
+++ leetcode367.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun isPerfectSquare(num: Int): Boolean {
+        var ans: Long = 0
+        while (ans * ans <= num.toLong())
+            ans++
+        return (ans - 1)*(ans-1)==num.toLong()
+    }
+}
