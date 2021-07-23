088b98199ba4d039fb7354e2890e0061a46ba17b
atul.yadav <atul.yadav@verse.in>
2021-07-23 17:46:49 +0530

    solution 633

---
+++ leetcode633.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun isSquare(a: Int): Boolean {
+        var b = Math.sqrt(a.toDouble()).toInt()
+        return b * b == a
+    }
+
+    fun judgeSquareSum(c: Int): Boolean {
+        var a = Math.sqrt(c.toDouble()).toInt()
+        for (i in 0 until a + 1) {
+            if (isSquare(c - i * i))
+                return true
+        }
+        return false
+    }
+}
