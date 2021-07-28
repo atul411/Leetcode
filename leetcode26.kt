725b898c3fa0cfd4d679f6e11a9e899ec964b91c
Atul Yadav <61058886+atul411@users.noreply.github.com>
2021-07-28 18:28:49 +0530

    Remove Duplicates from Sorted Array

--- leetcode26.kt
+++ leetcode26.kt
@@ -3,12 +3,11 @@ class Solution {
         var ans = if (nums.isEmpty()) 0 else 1
         for (i in 1 until nums.size) {
             if (nums[i] != nums[i - 1]) {
-                nums[i] = nums[ans].also { nums[ans] = nums[i] }
+                nums[ans] = nums[i]
                 ans++
             }
-
+           
         }
         return ans
     }
 }
-
