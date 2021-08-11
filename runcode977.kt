408111b2d541a42e325fac063bec120cb0a5ffdf
Atul Yadav <61058886+atul411@users.noreply.github.com>
2021-08-11 17:39:35 +0530

    Squares of a Sorted Array

---
+++ runcode977.kt
@@ -0,0 +1,39 @@
+class Solution {
+    fun sortedSquares(nums: IntArray): IntArray
+    {
+        var ans = nums.map { it*it }.toIntArray()
+        ans.sort()
+        return ans
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
