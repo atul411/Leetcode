69e4a6cffa671072cdf12172579ffc828c28be5a
Atul Yadav <61058886+atul411@users.noreply.github.com>
2021-08-12 10:46:49 +0530

    Valid Perfect Square

--- leetcode367.kt
+++ leetcode367.kt
@@ -2,7 +2,7 @@ class Solution {
     fun isPerfectSquare(num: Int): Boolean {
         var ans: Long = 0
         while (ans * ans <= num.toLong())
-            ans++
+            ans++;
         return (ans - 1)*(ans-1)==num.toLong()
     }
 }
