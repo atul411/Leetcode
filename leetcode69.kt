5301964d05781756aefb7f850f72ac1108d69898
Atul Yadav <61058886+atul411@users.noreply.github.com>
2021-08-12 10:48:12 +0530

    Sqrt(x)

--- leetcode69.kt
+++ leetcode69.kt
@@ -2,7 +2,7 @@ class Solution {
     fun mySqrt(x: Int): Int {
         var ans:Long = 0
         while (ans*ans<=x.toLong())
-            ans++
+            ans++;
         return (ans-1).toInt()
     }
 }
