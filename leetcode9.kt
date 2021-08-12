d8d1603a251d443d26c384488fd533f6003351fe
Atul Yadav <61058886+atul411@users.noreply.github.com>
2021-08-12 10:45:06 +0530

    Palindrome Number

--- leetcode9.kt
+++ leetcode9.kt
@@ -3,6 +3,6 @@ class Solution {
         var a = x.toString()
         var (start, end) = arrayOf(0, a.length - 1)
         while (start < end) if (a[start++] != a[end--]) return false
-        return true
+        return true;
     }
 }
