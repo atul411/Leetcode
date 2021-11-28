2657a62b6eff1e1d959443d65d566a8106464253
Atul Yadav <61058886+atul411@users.noreply.github.com>
2021-11-28 21:45:54 +0530

    ncreasing Triplet Subsequence

---
+++ leetcode334.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+	bool increasingTriplet(vector<int> &nums)
+	{
+		int n = nums.size(), low = INT_MAX, high = INT_MAX,i=0;
+		while (i<n)
+		{
+			if(low>=nums[i]){
+				low = nums[i++];
+			}
+			else if(high>=nums[i]){
+				high = nums[i++];
+			}
+			else
+				return true;
+		}
+		return false;
+		
+	}
+};
