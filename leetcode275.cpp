83d9719eee8184d330706cc892550900d8a24647
Atul Yadav <61058886+atul411@users.noreply.github.com>
2021-11-28 20:17:28 +0530

    H-Index II

---
+++ leetcode275.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size(), ans = -1;
+		for (int i = 0; i < n; i++)
+		{
+			if (n - i >= citations[i])
+			{
+				ans = max(ans,citations[i]);
+			}
+			else{
+                ans = max(ans, n-i);
+			}
+		}
+		return ans;
+    }
+};
