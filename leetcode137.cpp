03aa6d73c701ea8dddbf41d9ee4313b91598ad40
Atul Yadav <61058886+atul411@users.noreply.github.com>
2021-11-28 12:43:17 +0530

    Single Number II

---
+++ leetcode137.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+	int singleNumber(vector<int> &nums)
+	{
+		int ans;
+		unordered_map<int, int> umap;
+		for (int i : nums)
+		{
+			if (umap[i] == 2)
+			{
+				umap.erase(i);
+			}
+			else
+			{
+				umap[i]++;
+			}
+		}
+		for (auto i = umap.begin(); i != umap.end(); i++)
+		{
+			ans = i->first;
+		}
+		return ans;
+	}
+};
