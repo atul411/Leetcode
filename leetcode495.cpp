eddf424555c48e553e3e20e9b1ed1c33fb066209
Atul Yadav <61058886+atul411@users.noreply.github.com>
2022-09-03 00:06:10 -0700

    Teemo Attacking

---
+++ leetcode495.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findPoisonedDuration(vector<int> &timeSeries, int duration)
+    {
+        int ans = duration, size = timeSeries.size(), poisendUntil = timeSeries[0] + duration;
+        for (int i = 1; i < size; i++)
+        {
+            if (poisendUntil < timeSeries[i])
+            {
+                ans += duration;
+            }
+            else
+            {
+                ans + = timeSeries[i] + duration - poisendUntil;
+            }
+            poisendUntil = timeSeries[i] + duration;
+        }
+    }
+    return ans;
+};
