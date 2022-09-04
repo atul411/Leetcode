6499e9fa4e2a6cfc1811fc6eae12cf869af980d8
Atul Yadav <61058886+atul411@users.noreply.github.com>
2022-09-04 21:53:42 +0530

    Range Addition II

---
+++ leetode598.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxCount(int m, int n, vector<vector<int>> &ops)
+    {
+        int minRow = m, minCol = n;
+        for (int i = 0; i < ops.size(); i++)
+        {
+            minRow = min(minRow, ops[i][0]);
+            minCol = min(minCol, ops[i][1]);
+        }
+        return minRow * minCol;
+    }
+};
