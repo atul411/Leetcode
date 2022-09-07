eb350f25a31b65961a12cb18c2f775831e42d896
Atul Yadav <61058886+atul411@users.noreply.github.com>
2022-09-07 11:46:58 +0530

    Set Mismatch
    optimization

--- leetcode645.cpp
+++ leetcode645.cpp
@@ -6,26 +6,22 @@ class Solution
 public:
     vector<int> findErrorNums(vector<int> &nums)
     {
-        unordered_set<int> set;
-        vector<int> ans, sol(2, 0);
         int n = nums.size();
-        vector<int> arr(n, 0);
+        vector<int> arr(n, 0), sol(2, 0);
         for (int i = 0; i < n; i++)
         {
             arr[nums[i] - 1]++;
         }
         for (int i = 0; i < n; i++)
         {
-            switch (arr[i])
+            if (arr[i] == 2)
             {
-            case 2:
                 sol[0] = i + 1;
-                break;
-            case 0:
+            }
+
+            if (arr[i] == 0)
+            {
                 sol[1] = i + 1;
-                break;
-            default:
-                break;
             }
         }
         return sol;
