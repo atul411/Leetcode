ba9e62d04aee83c27849d73d53fc188796823684
Atul Yadav <61058886+atul411@users.noreply.github.com>
2022-09-08 16:45:59 +0530

    Course Schedule

---
+++ leetcode207.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    unordered_set<int> st;
+    unordered_map<int, vector<int>> mp;
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+    {
+        for (int i = 0; i < numCourses; i++)
+        {
+            mp[i] = vector(0,0);
+        }
+        for (auto i : prerequisites)
+        {
+            mp[i[0]].push_back(i[1]);
+        }
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (!dfs(i))
+                return false;
+        }
+        return true;
+    }
+    bool dfs(int x)
+    {
+        if (st.count(x))
+            return false;
+        if (mp[x].size() == 0)
+            return true;
+        st.insert(x);
+        for(auto i: mp[x]){
+            if(!dfs(i)) return false;
+        }
+        st.erase(x);
+        mp[x] = vector(0,0);
+        return true;
+    }
+};
