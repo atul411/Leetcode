43cd488def5a9c71e9a9abb8a7e176ef9332d293
Atul Yadav <61058886+atul411@users.noreply.github.com>
2022-09-08 14:10:15 +0530

    Isomorphic Strings

---
+++ leetcode205.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isIsomorphic(string s, string t)
+    {
+        unordered_map<char, char> keytovalue;
+        unordered_map<char, char> valuetokey;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (keytovalue.find(s[i]) != keytovalue.end())
+            {
+                if (keytovalue[s[i]] != t[i])
+                    return false;
+            }
+            else if (valuetokey.find(t[i]) != valuetokey.end())
+            {
+                return false;
+            }
+            else
+            {
+                keytovalue[s[i]] = t[i];
+                valuetokey[t[i]] = s[i];
+            }
+        }
+        return true;
+    }
+};
