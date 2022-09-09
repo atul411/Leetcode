7ef436c57cdd2a0328c1371b005fd4b8ff8cb519
Atul Yadav <61058886+atul411@users.noreply.github.com>
2022-09-09 13:07:32 +0530

    Basic Calculator II

---
+++ leetcode227.cpp
@@ -0,0 +1,75 @@
+class Solution
+{
+private:
+public:
+    int calculate(string s)
+    {
+        long long num = 0;
+        char sign = '+';
+        stack<long long> st;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (isspace(s[i]))
+                continue;
+            if (isdigit(s[i]))
+            {
+                num = num*10 + s[i] - '0';
+            }
+            else
+            {
+                if (sign == '+')
+                {
+                    st.push(num);
+                }
+                if (sign == '-')
+                {
+                    st.push(-num);
+                }
+                if (sign == '*')
+                {
+                    int last = st.top();
+                    st.pop();
+                    st.push(num * last);
+                }
+                if (sign == '/')
+                {
+                    int last = st.top();
+                    st.pop();
+                    st.push(last / num);
+                }
+                num = 0;
+                sign = s[i];
+            }
+        }
+        if (num != 0)
+        {
+            if (sign == '+')
+            {
+                st.push(num);
+            }
+            if (sign == '-')
+            {
+                st.push(-num);
+            }
+            if (sign == '*')
+            {
+                int last = st.top();
+                st.pop();
+                st.push(num * last);
+            }
+            if (sign == '/')
+            {
+                int last = st.top();
+                st.pop();
+                st.push(last / num);
+            }
+        }
+        long long ans = 0;
+        while (!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
